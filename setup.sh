57e95922ab1782508c8498f33ad0acbdf30617b1
ciseappsec <50307103+ciseappsec@users.noreply.github.com>
2019-08-25 13:24:56 -0700

    basic startup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+useradd -c "Merritt User" -d /home/merritt -s /usr/bin/bash -m merritt
+mkdir /home/merritt/public_html
+chmod 755 /home/merritt/public_html
+
+echo "let b:did_intent = 1" >> /home/merritt/.vim/indent/html.vim
+chmod 644 /home/merritt/.vim/indent/html.vim
+
+echo "let b:did_intent = 1" >> /home/root/.vim/indent/html.vim
+chmod 644 /home/root/.vim/indent/html.vim
+
+echo "merritt  ALL=(ALL:ALL) ALL" >>  /etc/sudoers
+
+a2enmod userdir
